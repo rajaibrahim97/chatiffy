431379f77b0c08625e25c74bcac6c33575bddc4a
rajaibrahim97 <ibi.ri19@gmail.com>
2025-10-22 06:45:42 -0700

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,25 @@ import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import path from "path";
 
 dotenv.config();
 
 const app = express()
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-
+// make ready fo deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    // use a RegExp catch-all (bypasses path-to-regexp string parser)
+    app.get(/.*/, (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+} 
 
 app.listen(PORT,()=>console.log("Server is running on port 3000"))
