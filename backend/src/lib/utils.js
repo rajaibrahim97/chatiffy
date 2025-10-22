3ff77cd24ac0a4df70044f668e7708142bc0000e
rajaibrahim97 <ibi.ri19@gmail.com>
2025-10-22 09:28:56 -0700

    sign up controller added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) =>{
+    // first generate the token for userId get the the jwt secret from env
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d",})
+    // sending back in form of cookie 
+    res.cookie("jwt",token,{
+        maxAge: 7 * 24 * 60 * 60 *1000, // MS
+        httpOnly: true, // prevent XSS attacks: cross-site scripting
+        sameSite: "strict", // CSRF attacks
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    })
+}
