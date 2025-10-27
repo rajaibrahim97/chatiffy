3ca0c61ff85ad4952f4fc60eba947d3682c426e0
rajaibrahim97 <ibi.ri19@gmail.com>
2025-10-28 00:25:33 +0500

    signup implemented on front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router'
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
 import SignUp from './pages/SignUpPage'
 import { useAuthStore } from '../lib/store/useAuthStore.js'
+import PageLoader from './components/PageLoader'
+import {Toaster} from "react-hot-toast";
 
 const App = () => {
-  const {authUser,isLoggedIn,login} = useAuthStore();
-  console.log('auth user:',authUser);
-  console.log("isLoggedIn: ",isLoggedIn)
+  const {checkAuth, isCheckingAuth, authUser} = useAuthStore();
   
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth])
+
+  if(isCheckingAuth) return <PageLoader/>
   
   return (
     <div className="min-h-screen  bg-slate-900 relative flex items-center justify-center p-4 overfllow-hidden">
@@ -17,11 +22,11 @@ const App = () => {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-    <button className='z-10' onClick={login}>LoggedIn</button>
+    
       <Routes>
-        <Route path="/" element={<ChatPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/" element={authUser ? <ChatPage />: <Navigate to="/login"/>} ></Route>
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>}></Route>
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to={"/"}/>}></Route>
       </Routes>
     </div>
   )
